e811539c144fee8e1943484bb14eab89610dc762
naufal <askpradana@gmail.com>
2023-09-18 11:41:51 +0700

    refactor(file): refactor all app files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
-import EditPage from "./temp/editpage/EditPage";
-import HomePage from "./temp/homepage/HomePage";
-import LoginPage from "./temp/loginpage/LoginPage";
+import EditPage from "./pages/editpage/EditPage";
+import HomePage from "./pages/homepage/HomePage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -9,8 +8,7 @@ function App() {
 		<Router>
 			<div>
 				<Routes>
-					<Route exact path="/" Component={LoginPage} />
-					<Route path="/home" Component={HomePage} />
+					<Route exact path="/" Component={HomePage} />
 					<Route path="/edit" Component={EditPage} />
 				</Routes>
 			</div>
